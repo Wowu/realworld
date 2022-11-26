6da26a3bcdc71a10c010be6ae469bcfa33a936b2
Karol Musur <kamusur@gmail.com>
2022-11-26 19:36:54 +0100

    Add * to allowed hosts

--- realworld/settings.py
+++ realworld/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-f35(x7w#1hz7%oejc(t(x8ii7n^%n0pvzsp@x*qtfh8^$3^3j+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["http://*", "http://*.knbit.edu.pl"]
 
 
 # Application definition
